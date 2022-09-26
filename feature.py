f18c412228bce13f54d71f01173c954e1d2c696c
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-09-26 16:26:21 +0500

    update in feature transformer

--- feature.py
+++ feature.py
@@ -1,12 +1,15 @@
 from pyspark.sql import SparkSession
-import pyspark.sql.functions as F
-from pyspark.sql.window import Window
-from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from feature_transformers.log_transformer import LogTransformer
 from feature_transformers.lag_feature import LagTransformer
 
 spark = SparkSession.builder.master("local[5]").appName('Feature Engineering').getOrCreate()
-df = spark.read.csv("../Dataset/train.csv", header=True, inferSchema=True)
+df = spark.read.csv("../Dataset/train-test/train.csv", header=True, inferSchema=True)
 df.repartition(5)
 
+log_transformer = LogTransformer()
+log_df = log_transformer.transform(df)
+log_df.show()
+
 lag_transformer = LagTransformer(4)
-transformed_df = lag_transformer.transform(df)
+transformed_df = lag_transformer.transform(log_df)
+transformed_df.show()
