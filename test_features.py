bb94e6b23dbeb511a4290dab8d4abe57599bde7d
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-09-26 19:17:33 +0500

    - lag transformation unittest completed

--- test_features.py
+++ test_features.py
@@ -3,11 +3,10 @@ from pyspark.sql.session import SparkSession
 import math
 from feature_transformers.log_transformer import LogTransformer
 from feature_transformers.lag_feature import LagTransformer
-
+import pyspark.sql.functions as F
 
 
 class MyTestCase(unittest.TestCase):
-    agg_df = None
 
     def test_log_transformer(self):
         columns = ["sales"]
@@ -22,8 +21,15 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(cal_data, [math.log10(x) for x in data])
 
 
+    def test_lag_trasformer(self):
+        spark = SparkSession.builder.master("local[5]").appName('MLE Test Case').getOrCreate()
+        df = spark.read.csv('test_dataset/sales_data.csv', inferSchema=True, header=True)
 
-
+        lag_transformer = LagTransformer(2)
+        df = lag_transformer.transform(df)
+        trans = df.select(F.count(F.when(F.isnan('sales') | F.col('sales').isNull(), 'sales')).alias('sales'))
+        count = trans.select('sales').rdd.flatMap(lambda x: x).collect()[0]
+        self.assertGreater(count, 0)
 
 
 if __name__ == '__main__':
