48f92d2c3e347c4bba04aeb098f4cb4ab134557c
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-09-26 13:33:47 +0500

    - update train_test transformer with groupby.applyInPandas function

--- transformers/train_test_transformer.py
+++ transformers/train_test_transformer.py
@@ -1,18 +1,46 @@
 import pyspark.sql.functions as F
 from pyspark.sql.session import SparkSession
+import pandas as pd
 from pyspark.ml import Transformer
 import math
 import warnings
 import functools
 
 
+
 warnings.filterwarnings('ignore')
+
+
+def g(df):
+    count = df['store_id'].count()
+    df['split'] = 'train'
+    df['index'] = range(0, count)
+
+    count = df['store_id'].count()
+    train_ratio = math.floor(count * 0.7)
+    df.loc[df['index'] >= train_ratio, 'split'] = 'test'
+    df.drop('index', axis=1, inplace=True)
+    return df
 
 
 class TrainTestTransformer(Transformer):
 
     def __init__(self):
         self.spark = SparkSession.builder.master("local[5]").appName('MLE Assignment').getOrCreate()
+        self.schema =  """
+            store_id string,
+            dept_id string,
+            date string,
+            demand double,
+            wm_yr_wk double,
+            wday double,
+            month double,
+            year double,
+            sell_price double,
+            sales double,
+            flag int,
+            split string
+        """
 
     def unionAll(self, dfs):
         if len(dfs[0].columns) != 0:
@@ -21,33 +49,10 @@ class TrainTestTransformer(Transformer):
             return dfs[1]
 
     def _transform(self, df):
-        data = [()]
-        final_train_df = self.spark.createDataFrame(data)
-        final_test_df = self.spark.createDataFrame(data)
-
-        stores = df.select('store_id').distinct().orderBy('store_id').collect()
-        depts = df.select('dept_id').distinct().orderBy('dept_id').collect()
-        dataset_count = df.where((df.store_id == 'CA_1') & (df.dept_id == 'FOODS_1')).count()
-        train_ratio = math.floor(dataset_count * 0.7)
-
-        for store in stores:
-            for dept in depts:
-                dataset = df.where((df.store_id == store['store_id']) & (df.dept_id == dept['dept_id']))
-
-                # adding new column named index for split
-                dataset = dataset.withColumn("index", F.monotonically_increasing_id())
-
-                # computing train test ratio
-
-                # train test split
-                training_df = dataset.filter(dataset.index < train_ratio)
-                testing_df = dataset.filter(dataset.index >= train_ratio)
-
-                training_df = training_df.drop(F.col('index'))
-                testing_df = testing_df.drop(F.col('index'))
-
-                final_train_df = self.unionAll([final_train_df, training_df])
-                final_test_df = self.unionAll([final_test_df, testing_df])
-
-        return final_train_df, final_test_df
+        split_df = df.groupBy(['store_id', 'dept_id']).applyInPandas(g, schema=self.schema)
+        cols = split_df.columns
+        cols.remove('split')
+        train = split_df.where(split_df.split == "train").select(cols)
+        test = split_df.filter(split_df.split == "test").select(cols)
+        return train, test
 
