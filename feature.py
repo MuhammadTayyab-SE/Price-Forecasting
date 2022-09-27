41bdf78f0c52e0b6600f17818f91fa6d44df361c
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-09-27 13:00:25 +0500

    - transformation of test data added

--- feature.py
+++ feature.py
@@ -3,13 +3,28 @@ from feature_transformers.log_transformer import LogTransformer
 from feature_transformers.lag_feature import LagTransformer
 
 spark = SparkSession.builder.master("local[5]").appName('Feature Engineering').getOrCreate()
-df = spark.read.csv("../Dataset/train-test/train.csv", header=True, inferSchema=True)
-df.repartition(5)
+
+# feature transformation on train data
+df_train = spark.read.csv("../Dataset/train-test/train.csv", header=True, inferSchema=True)
+df_train.repartition(5)
 
 log_transformer = LogTransformer()
-log_df = log_transformer.transform(df)
-log_df.show()
+log_df_train = log_transformer.transform(df_train)
+log_df_train.show()
 
 lag_transformer = LagTransformer(4)
-transformed_df = lag_transformer.transform(log_df)
-transformed_df.show()
+transformed_df_train = lag_transformer.transform(log_df_train)
+transformed_df_train.show()
+
+# feature transformation on test data
+
+df_test = spark.read.csv("../Dataset/train-test/test.csv", header=True, inferSchema=True)
+df_test.repartition(5)
+
+log_transformer = LogTransformer()
+log_df_test = log_transformer.transform(df_test)
+log_df_test.show()
+
+lag_transformer = LagTransformer(4)
+transformed_df_test = lag_transformer.transform(log_df_test)
+transformed_df_test.show()
