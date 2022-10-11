df18d544a06d2b642224b8d3db44b7b2cd3b1238
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-10-11 15:36:02 +0500

    - update log and lag feature transformers

--- feature.py
+++ feature.py
@@ -5,10 +5,10 @@ from feature_transformers.lag_feature import LagTransformer
 spark = SparkSession.builder.master("local[5]").appName('Feature Engineering').getOrCreate()
 
 # feature transformation on train data
-df_train = spark.read.csv("../Dataset/train-test/train.csv", header=True, inferSchema=True)
+df_train = spark.read.csv("../Dataset/train.csv", header=True, inferSchema=True)
 df_train.repartition(5)
 
-log_transformer = LogTransformer()
+log_transformer = LogTransformer(column=['sales'])
 log_df_train = log_transformer.transform(df_train)
 log_df_train.show()
 
@@ -18,10 +18,10 @@ transformed_df_train.show()
 
 # feature transformation on test data
 
-df_test = spark.read.csv("../Dataset/train-test/test.csv", header=True, inferSchema=True)
+df_test = spark.read.csv("../Dataset/test.csv", header=True, inferSchema=True)
 df_test.repartition(5)
 
-log_transformer = LogTransformer()
+log_transformer = LogTransformer(column=['sales'])
 log_df_test = log_transformer.transform(df_test)
 log_df_test.show()
 
--- feature_transformers/log_transformer.py
+++ feature_transformers/log_transformer.py
@@ -1,17 +1,26 @@
 import pyspark.sql.functions as f
 from pyspark.ml import Transformer
 import warnings
+from pyspark.ml.param.shared import HasInputCols, HasOutputCols
 warnings.filterwarnings('ignore')
 
 
-class LogTransformer(Transformer):
-    def __init__(self):
+class LogTransformer(Transformer, HasInputCols, HasOutputCols):
+    def __init__(self, column: list):
         super().__init__()
-        pass
+        self.columns = column
 
     def _transform(self, df):
-        # Ignore this line
-        self.none = None
+        """
+        Gets a pyspark dataframe and return dataframe with log transformation on given columns
+        :param df: dataframe
+        :return: dataframe with log transformed columns
+        """
+        column_list = [x[0] for x in df.dtypes]
 
-        df = df.withColumn('sales', f.log10(f.col('sales')))
+        for column in self.columns:
+            if column in column_list:
+                df = df.withColumn(column, f.log10(f.col(column)))
+            else:
+                print(f"{column} not in the list of columns {column_list}")
         return df
