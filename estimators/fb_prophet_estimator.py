e9bc127d29fbf66ea340ff9714dfad5f5d632ffb
Muhammad Tayyab 01 <muhammad.tayyab01@confiz.local>
2022-10-24 11:08:54 +0500

    - update prophet model

--- estimators/fb_prophet_estimator.py
+++ estimators/fb_prophet_estimator.py
@@ -2,10 +2,11 @@ from prophet import Prophet
 from pyspark.ml import Estimator
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import warnings
 from hyperopt import hp
 from estimators.tune_fb_prophet import Tune
+import os
+import joblib
 
 warnings.filterwarnings('ignore')
 
@@ -37,50 +38,90 @@ class ProphetEstimator(Estimator):
         # self.schema = """ds date,y double"""
 
     def _fit(self, dataset):
+        # changing columns according to prophet model requirements
         dataset = change_col_names(dataset, self.target_col)
+
+        # extract train and test dataset from single dataframe
         df_train = dataset.filter(dataset.split == "train")
         df_test = dataset.filter(dataset.split == 'test').toPandas()
 
         def forecast_store_item(df):
+            # create dataset according to training dataset
             test = df_test[df_test.store_id == df.store_id.unique()[0]]
             test = test[df_test.dept_id == df.dept_id.unique()[0]]
             test = test.iloc[:365, :]
+
+            # predictions_df will be used to return predictions from the function
+            predictions_df = test[['ds', 'store_id', 'dept_id', 'y']].copy()
+            predictions_df.reset_index(inplace=True)
+
+            # extracting columns needed for training of dataset
             df = df[['ds', 'y']]
+
+            # save_df dataframe will be used to save scores into .csv file
+            save_df = test.head(1)[['store_id', 'dept_id']]
+            print(test)
 
             temp_df = df.copy()
             temp_test = test.copy()
 
+            # defining space for hyperopt
             space = {
                 'seasonality_mode': hp.choice('seasonality_mode', ['multiplicative', 'additive']),
                 'changepoint_prior_scale': hp.choice('changepoint_prior_scale', np.arange(.1, 0.9, .1)),
                 'changepoint_range': hp.choice('changepoint_range', np.arange(.1, 0.8, .1)),
-                'daily_seasonality': hp.choice('daily_seasonality', [True]),
+                'daily_seasonality': hp.choice('daily_seasonality', [True, False]),
                 'yearly_seasonality': hp.choice('yearly_seasonality', [True]),
                 'weekly_seasonality': hp.choice('weekly_seasonality', [True]),
                 'seasonality_prior_scale': hp.choice('seasonality_prior_scale', np.arange(1, 10, 1)),
             }
-
+            #  tuning hyper-parameters
             tune = Tune(temp_df, temp_test, space)
             params = tune.tune_hyper_parameters()
 
+            # train final model on tuned hps
             model = Prophet(**params)
             model.fit(df)
 
+            # making future predictions
             daily_model_forecast_future_data = model.make_future_dataframe(periods=365, freq='D', include_history=False)
             daily_model_forecast = model.predict(daily_model_forecast_future_data)
-
-            print(f"Daily Model Store: {test.store_id.unique()[0]}, Dept:{test.dept_id.unique()[0]}")
-            print(f"Mean Squared Error: {mean_squared_error(test.y, daily_model_forecast.yhat)}")
-            print(f"R2 Score: {r2_score(test.y, daily_model_forecast.yhat)}")
-            print(f"Mean Absolute Error: {mean_absolute_error(test.y, daily_model_forecast.yhat)}")
-            print(f"""Mean Absolute Percentage Error: 
-                    {mean_absolute_percentage_error(test.y, daily_model_forecast.yhat)} \n""")
-            test_params = test[['ds', 'store_id', 'dept_id', 'y']]
-            test_params.reset_index(inplace=True)
             daily_model_forecast.reset_index(inplace=True)
-            test_params['yhat'] = pd.Series(daily_model_forecast['yhat'].to_list())
+            predictions_df['yhat'] = pd.Series(daily_model_forecast['yhat'].to_list())
 
-            return test_params[['ds', 'store_id', 'dept_id', 'y', 'yhat']]
+            #  Calculating MAPE on daily bases
+            df_ = predictions_df.copy()
+
+            # taking anti-log of values for prediction
+            df_['y'] = np.exp(df_.y)
+            df_['yhat'] = np.exp(df_.yhat)
+
+            daily_mape = mean_absolute_percentage_error(df_.y.copy(), df_.yhat.copy())
+            print(f"Daily Model Store: {test.store_id.unique()[0]}, Dept:{test.dept_id.unique()[0]}")
+            print(f"""Mean Absolute Percentage Error: 
+                    {daily_mape} \n""")
+            save_df['daily'] = daily_mape
+
+            # calculating monthly MAPE
+            df_.ds = pd.to_datetime(df_.ds)
+            df_.set_index('ds', inplace=True)
+            df_ = df_.resample('M').sum()
+            monthly_mape = mean_absolute_percentage_error(df_['y'].copy(), df_['yhat'].copy())
+            save_df['monthly'] = monthly_mape
+            print(f"""Monthly MAPE: {monthly_mape} for store: {test.store_id.unique()[0]}
+             & dept {test.dept_id.unique()[0]}""")
+
+            # saving score in .csv file
+            if os.path.exists("../models/prophet/MAPE.csv"):
+                save_df.to_csv("../models/prophet/MAPE.csv", mode='a', index=False, header=False)
+            else:
+                save_df.to_csv("../models/prophet/MAPE.csv", mode='w', index=False)
+
+            filename = "../models/prophet/" + predictions_df.head(1)['store_id'].tolist()[0] + '-' + \
+                       predictions_df.head(1)['dept_id'].tolist()[0] + '.pkl'
+            joblib.dump(model, filename)
+
+            return predictions_df[['ds', 'store_id', 'dept_id', 'y', 'yhat']]
 
         results = df_train.groupby(['store_id', 'dept_id']).applyInPandas(forecast_store_item, schema=self.schema)
         return results
